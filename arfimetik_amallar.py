2392da5300c58818e5c96726ef933bdcb2652463
Doston Togaev <togayevd37@gmail.com>
2024-04-22 00:08:38 +0500

    yuza va parametr

---
+++ arfimetik_amallar.py
@@ -0,0 +1,12 @@
+from math import pi
+a = float(input("tomonni kiritng: "))
+
+b = float(input("tomonni kiritng: "))
+
+P = 2 * (a + b )
+
+print('peremetiri teng: ',P)
+
+S = (a * b)
+
+print('yuzasi teng: ',S)
